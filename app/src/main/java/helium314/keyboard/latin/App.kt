b2529872cd2bd0e35f10a043798aba530218072f
LeanBitLab <leanbitlab@users.noreply.github.com>
2026-07-12 23:50:51 +0530

    feat: add diagnostic logging for system emoji toggling

--- app/src/main/java/helium314/keyboard/latin/App.kt
+++ app/src/main/java/helium314/keyboard/latin/App.kt
@@ -25,11 +25,18 @@ class App : Application(), Configuration.Provider {
         super.onCreate()
         DebugFlags.init(this)
         Settings.init(this)
-        if (!Settings.getInstance().useSystemEmoji()) {
+        val useSystemEmoji = Settings.getInstance().useSystemEmoji()
+        Log.i("EmojiCompatInit", "useSystemEmoji is $useSystemEmoji")
+        if (!useSystemEmoji) {
+            Log.i("EmojiCompatInit", "Initializing EmojiCompat...")
             val config = DefaultEmojiCompatConfig.create(this)
             if (config != null) {
                 EmojiCompat.init(config)
+            } else {
+                Log.w("EmojiCompatInit", "DefaultEmojiCompatConfig returned null")
             }
+        } else {
+            Log.i("EmojiCompatInit", "Skipping EmojiCompat (using system emoji font)")
         }
         SubtypeSettings.init(this)
         RichInputMethodManager.init(this)
